aff646651267163cb402bf90a31e4c04a78efecd
mahesh yadav <mahesh.yadav@clouddrove.com>
2022-12-12 18:37:19 +0530

    inital commit

--- _module/azure-labels/_example/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = module.labels.id
-  description = "Disambiguated ID."
-}
-
-output "tags" {
-  value       = module.labels.tags
-  description = "Normalized Tag map."
-}
--- _module/azure-vm/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "nic_name" {
-  value = azurerm_network_interface.nic.*.name
-}
-
-output "virtual_machine_name" {
-  value = azurerm_linux_virtual_machine.linux_vm.*.name
-}
-
